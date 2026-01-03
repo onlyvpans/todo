d55d0a06a80361dc8cad08ba24d50fe3c6812f46
viv <vivpan95@gmail.com>
2026-01-04 03:54:57 +1100

    feat(ui): set up required tabs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,3 +1,14 @@
 export function Tabs() {
-  return <nav></nav>;
+  const tabs = ["All", "Open", "Completed"];
+  return (
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        return (
+          <button className="tab-button" key={tabIndex}>
+            {tab} <span>(0)</span>
+          </button>
+        );
+      })}
+    </nav>
+  );
 }
