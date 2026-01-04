f32db803d2b98b34b87405b58c9d0437db50ca97
viv <vivpan95@gmail.com>
2026-01-04 20:48:05 +1100

    feat: create tabs component to track all, open, closed tasks

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,11 +1,25 @@
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+
 export function Tabs() {
+  const todos = useContext(TodoContext);
+
   const tabs = ["All", "Open", "Completed"];
+  console.log(todos);
+
   return (
     <nav className="tab-container">
       {tabs.map((tab, tabIndex) => {
+        const numOfTasks =
+          tab === "All"
+            ? todos.length
+            : tab === "Open"
+            ? todos.filter((val) => !val.complete).length
+            : todos.filter((val) => val.complete).length;
+
         return (
           <button className="tab-button" key={tabIndex}>
-            {tab} <span>(0)</span>
+            {tab} <span>{numOfTasks}</span>
           </button>
         );
       })}
