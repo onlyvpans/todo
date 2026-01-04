502f3d73a59e79c975324579c18c1ef316c7a043
viv <vivpan95@gmail.com>
2026-01-05 03:09:21 +1100

    feat: show user which tabs are selected

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,11 +1,16 @@
-import { useContext } from "react";
+import { useContext, type SetStateAction } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 
-export function Tabs() {
-  const todos = useContext(TodoContext);
+type tabEnum = "All" | "Open" | "Completed";
 
-  const tabs = ["All", "Open", "Completed"];
-  console.log(todos);
+interface Tabs {
+  selectedTab: tabEnum;
+  setSelectedTab: React.Dispatch<SetStateAction<tabEnum>>;
+}
+
+export function Tabs({ selectedTab, setSelectedTab }: Tabs) {
+  const todos = useContext(TodoContext);
+  const tabs: tabEnum[] = ["All", "Open", "Completed"];
 
   return (
     <nav className="tab-container">
@@ -18,7 +23,16 @@ export function Tabs() {
             : todos.filter((val) => val.complete).length;
 
         return (
-          <button className="tab-button" key={tabIndex}>
+          <button
+            className={
+              tab === selectedTab ? "tab-button tab-selected" : "tab-button"
+            }
+            key={tabIndex}
+            value={tab}
+            onClick={() => {
+              setSelectedTab(tab);
+            }}
+          >
             {tab} <span>{numOfTasks}</span>
           </button>
         );
