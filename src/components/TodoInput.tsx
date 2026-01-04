8bcde9fd7f4bf91fdd298c1a7c1a1fcedaf32d5c
viv <vivpan95@gmail.com>
2026-01-04 22:42:29 +1100

    feat: handle add dynamic todos to list

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,8 +1,26 @@
-export function TodoInput() {
+import { useState } from "react";
+
+interface handleToDoProp {
+  handleAddTodo: (newTodo: string) => void;
+}
+export function TodoInput({ handleAddTodo }: handleToDoProp) {
+  const [inputValue, setInputValue] = useState("");
+
+  console.log(inputValue);
   return (
     <div className="input-container">
-      <input placeholder="Add task" />
-      <button>
+      <input
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        placeholder="Add task"
+      />
+      <button
+        onClick={() => {
+          handleAddTodo(inputValue);
+        }}
+      >
         <i className="fa-solid fa-plus"></i>
       </button>
     </div>
