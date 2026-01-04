c65b0971f5e25a4e3969c510f4ba94de6b35ffe7
viv <vivpan95@gmail.com>
2026-01-04 22:42:46 +1100

    feat: handle filter todo list based on btns

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,26 @@
+import { TodoCard } from "./TodoCard";
+import { TodoContext } from "../contexts/TodoContext";
+import { useContext } from "react";
+
 export function TodoList() {
-  return <div></div>;
+  // const [todolist, setTodoList] = useState([]);
+
+  const todos = useContext(TodoContext);
+
+  const tab = "All";
+
+  const filterTodosList =
+    tab === "All"
+      ? todos
+      : tab === "open"
+      ? todos.filter((val) => !val.complete)
+      : todos.filter((val) => val.complete);
+
+  return (
+    <>
+      {filterTodosList.map((todo, todoIndex) => {
+        return <TodoCard key={todoIndex} {...todo}></TodoCard>;
+      })}
+    </>
+  );
 }
