9d0ce49431cf7cd6996641462127ac1f59de637d
viv <vivpan95@gmail.com>
2026-01-04 21:45:44 +1100

    feat: add buttons to todo card

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+interface TodoCardProps {
+  input: string;
+  complete: boolean;
+}
 
-export function TodoCard() {
-  return <div></div>;
+export function TodoCard({ input, complete }: TodoCardProps) {
+  return (
+    <div className="card todo-item">
+      <p>{input}</p>
+      <div>
+        <button disabled={complete}>
+          <h6>Done</h6>
+        </button>
+        <button>
+          <h6>Delete</h6>
+        </button>
+      </div>
+    </div>
+  );
 }
