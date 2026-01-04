558cca2010b4647ca50510331ca112faa8dd5b0c
viv <vivpan95@gmail.com>
2026-01-04 21:46:01 +1100

    feat: add input and button to todo input

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,3 +1,10 @@
 export function TodoInput() {
-  return <div></div>;
+  return (
+    <div className="input-container">
+      <input placeholder="Add task" />
+      <button>
+        <i className="fa-solid fa-plus"></i>
+      </button>
+    </div>
+  );
 }
